2432cfdb2360b93d06674e0c125b9076225f3507
Matheo_MSA <matheo.msa34@gmail.com>
2017-02-07 13:17:53 +0100

    ViewPager working

--- app/src/main/java/eu/epitech/fernan_s/msa_m/yourimage/activity/ImageActivity.java
+++ app/src/main/java/eu/epitech/fernan_s/msa_m/yourimage/activity/ImageActivity.java
@@ -1,10 +1,12 @@
 package eu.epitech.fernan_s.msa_m.yourimage.activity;
 
 import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -18,12 +20,16 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import eu.epitech.fernan_s.msa_m.yourimage.R;
+import eu.epitech.fernan_s.msa_m.yourimage.adapter.ScreenSlidePagerAdapter;
 import eu.epitech.fernan_s.msa_m.yourimage.model.image.IImage;
 import eu.epitech.fernan_s.msa_m.yourimage.model.thread.FlickrThread;
 import eu.epitech.fernan_s.msa_m.yourimage.model.thread.IThread;
 import eu.epitech.fernan_s.msa_m.yourimage.model.thread.ImgurThread;
 
 public class ImageActivity extends AppCompatActivity {
+    private ViewPager mPager;
+    private Context _ctx;
+    private ScreenSlidePagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +49,56 @@ public class ImageActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        final ImageView imageView = (ImageView) findViewById(R.id.tmp_image);
-        final Context _ctx = this;
+//        final ImageView imageView = (ImageView) findViewById(R.id.tmp_image);
+        _ctx = this;
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.viewPager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+
+        final ImageView imageView = (ImageView) mPager.findViewById(R.id.tmp_image);
+        //mPagerAdapter.setLThread(thread);
         thread.getImages(new IImage.getImageCallback() {
+            @Override
+            public void onGetImageFinished(List<IImage> lThread) {
+                String uri = lThread.get(0).getLink();
+                Log.d("LOL", "finished");
+                mPagerAdapter.setLThread(lThread);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPager.setAdapter(mPagerAdapter);
+                    }
+                });
+
+            }
+        });
+
+        // Attach the page change listener inside the activity
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(ImageActivity.this,
+                        "Selected page position: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Code goes here
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Code goes here
+            }
+        });
+
+/*        thread.getImages(new IImage.getImageCallback() {
             @Override
             public void onGetImageFinished(final List<IImage> lThread) {
                 String uri = lThread.get(0).getLink();
@@ -73,6 +126,6 @@ public class ImageActivity extends AppCompatActivity {
                     }
                 });
             }
-        });
+        });*/
     }
 }
---
+++ app/src/main/java/eu/epitech/fernan_s/msa_m/yourimage/adapter/ScreenSlidePagerAdapter.java
@@ -0,0 +1,46 @@
+package eu.epitech.fernan_s.msa_m.yourimage.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.Log;
+
+import java.util.List;
+
+import eu.epitech.fernan_s.msa_m.yourimage.ScreenSlidePageFragment;
+import eu.epitech.fernan_s.msa_m.yourimage.model.image.IImage;
+
+/**
+ * Created by matheo msa on 06/02/2017.
+ */
+
+public class ScreenSlidePagerAdapter  extends FragmentStatePagerAdapter {
+    int NUM_PAGES = 1;
+     List<IImage> _lThread = null;
+
+
+    public ScreenSlidePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void setLThread(List<IImage> lThread){
+        _lThread = lThread;
+        NUM_PAGES = lThread.size();
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        if (_lThread != null)
+            return ScreenSlidePageFragment.newInstance(position, _lThread.get(position).getTitle(), _lThread.get(position).getLink());
+        else
+            return new ScreenSlidePageFragment();
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return NUM_PAGES;
+    }
+}
