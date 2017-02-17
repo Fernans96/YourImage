54e7bbf686b39e4da21db87c4a53d5ddfb7a4995
Quentin Fernandez <quentin.fernandez@epitech.eu>
2017-02-17 15:57:16 +0100

    Fixed travis

--- app/src/androidTest/java/eu/epitech/fernan_s/msa_m/yourimage/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/eu/epitech/fernan_s/msa_m/yourimage/ExampleInstrumentedTest.java
@@ -2,31 +2,23 @@ package eu.epitech.fernan_s.msa_m.yourimage;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.annotation.UiThreadTest;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
-import android.widget.ImageView;
 
 import com.orm.SugarContext;
 
-import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.internal.MethodSorter;
 import org.junit.runner.RunWith;
 
-import java.net.URI;
 import java.util.List;
 
 import eu.epitech.fernan_s.msa_m.yourimage.model.api.IApi;
 import eu.epitech.fernan_s.msa_m.yourimage.model.api.PixivAPI;
 import eu.epitech.fernan_s.msa_m.yourimage.model.image.IImage;
 import eu.epitech.fernan_s.msa_m.yourimage.model.thread.PixivThread;
-import eu.epitech.fernan_s.msa_m.yourimage.tools.ImagesTools;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -80,17 +72,6 @@ public class ExampleInstrumentedTest {
         List<IImage> lim = lpix.get(0).sync_getImages();
         if (lim.size() < 1)
             throw new Exception("Image list empty");
-    }
-
-    @Test
-    public void ThreadFav() throws Exception {
-        List<PixivThread> lpix = new PixivAPI(InstrumentationRegistry.getTargetContext()).Sync_GetThread(0);
-        if (lpix.size() < 1)
-            throw new Exception("Thread list empty");
-        for (PixivThread pix : lpix) {
-            if (!pix.isFav())
-                pix.fav();
-        }
     }
 
     @After
